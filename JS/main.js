350186865faba7ce72666c23cb5812ee9eda7e73
Vincent Kipngetich <vinniebluz@icloud.com>
2022-09-20 02:41:36 +0300

    Used Javascript Click handler to toggle active css class in nav items

--- JS/main.js
+++ JS/main.js
@@ -12,18 +12,39 @@ const menuBtn = document.querySelector("#menu-btn");
 const closeBtn = document.querySelector("#close-btn");
 const menu = document.querySelector("nav .container ul");
 
-
 // open sidebar
 menuBtn.addEventListener("click", () => {
   menu.style.display = "block";
   menuBtn.style.display = "none";
   closeBtn.style.display = "inline-block";
-})
+});
 
 // close sidebar
 
 closeBtn.addEventListener("click", () => {
   menu.style.display = "none";
   closeBtn.style.display = "none";
-  menuBtn.style.display="inline-block";
-})
+  menuBtn.style.display = "inline-block";
+});
+
+// Change active class to cliked nav item
+
+const navItems = document.querySelectorAll("nav ul li");
+
+// Add active class to clike nav item
+// remove active class from other items
+
+const removeActiveClass = () => {
+  navItems.forEach((item) => {
+    const link = item.querySelector("a");
+    link.classList.remove("active");
+  });
+};
+
+navItems.forEach((item) => {
+  const link = item.querySelector("a");
+  link.addEventListener("click", () => {
+    removeActiveClass();
+    link.classList.add("active");
+  });
+});
