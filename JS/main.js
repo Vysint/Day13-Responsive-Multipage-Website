02a56f77db42e082dd72e934e71568c220a738dc
Vincent Kipngetich <vinniebluz@icloud.com>
2022-09-20 02:31:03 +0300

    Added Javascript click event handler to toggle the sidebar in Tablets and Mobiles

--- JS/main.js
+++ JS/main.js
@@ -1,7 +1,29 @@
 // Swiper js
 var swiper = new Swiper(".mySwiper", {
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    }
-  });
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+// open and close nav menu
+
+const menuBtn = document.querySelector("#menu-btn");
+const closeBtn = document.querySelector("#close-btn");
+const menu = document.querySelector("nav .container ul");
+
+
+// open sidebar
+menuBtn.addEventListener("click", () => {
+  menu.style.display = "block";
+  menuBtn.style.display = "none";
+  closeBtn.style.display = "inline-block";
+})
+
+// close sidebar
+
+closeBtn.addEventListener("click", () => {
+  menu.style.display = "none";
+  closeBtn.style.display = "none";
+  menuBtn.style.display="inline-block";
+})
